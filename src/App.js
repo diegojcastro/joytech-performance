a9b2deea8d1df299126d3788f41d3ed863d1f831
Diego <diegojcr4@gmail.com>
2022-01-16 15:08:32 -0500

    fix navbar boilerplate

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { useState } from 'react';
 import './App.css';
-
+import { Navbar } from './components/Navbar';
 function App() {
+  const [activeTab, setActiveTab] = useState('Editor');
+
   return (
     <div className="App">
+      <Navbar activeTab={activeTab} setActiveTab={setActiveTab}/>
       <p>Hello</p>
     </div>
   );
