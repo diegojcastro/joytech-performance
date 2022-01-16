d5ec5e9ac94311740a3854a777472d1ee75e2e56
Diego <diegojcr4@gmail.com>
2022-01-16 15:47:59 -0500

    add trash button for performance line

--- src/components/PerformanceLine.jsx
+++ src/components/PerformanceLine.jsx
@@ -9,17 +9,28 @@ export default function PerformanceLine(props) {
           Type
         </button>
         <ul className="dropdown-menu">
-          <li className="dropdown-item">A</li>
-          <li className="dropdown-item">B</li>
+          <li className="dropdown-item">Illusion</li>
+          <li className="dropdown-item">Command</li>
         </ul>
       </div>
 
       <div className="btn-group">
         <button className="btn btn-secondary btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-          Small button
+          Color
         </button>
         <ul className="dropdown-menu">
           ...
+        </ul>
+      </div>
+
+      <p>I'll put an input field here</p>
+      
+      <div className="btn-group">
+        <button className="btn btn-danger btn-sm" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+          🗑
+        </button>
+        <ul className="dropdown-menu">
+          <li className="dropdown-item">Delete This Line</li>
         </ul>
       </div>
     </div>
